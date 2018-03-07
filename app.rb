9f58e2176bf1bcebf4587f3f518e42cf6b60314a
Chris Cardle <cdcardle@gmail.com>
2018-03-07 15:22:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,8 +15,8 @@ module FormsLab
       pirate = params[pirate]
       @pirate = Pirate.new(pirate[name], pirate[weight], pirate[height])
       ships = pirate[ships]
-      @ship1 = Ship.new(ships[0][name], ships[0][type], ships[0]booty)
-      @ship2 = Ship.new(ships[1][name], ships[1][type], ships[1]booty)
+      @ship1 = Ship.new(ships[0][name], ships[0][type], ships[0][booty])
+      @ship2 = Ship.new(ships[1][name], ships[1][type], ships[1][booty])
       erb :"pirates/show"
     end
 
